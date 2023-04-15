3e69d0dd1923a4add55bf2eba3e130f656f62d40
Amirfery <Amirferyg@gmail.com>
2023-04-15 17:01:49 +0330

    add lcd

--- Connector/src/main.cpp
+++ Connector/src/main.cpp
@@ -1,14 +1,19 @@
 #include <Arduino.h>
 #include <string.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal LCD(12, 11, 5, 4, 3, 2);
 
 void setup() {
   Serial.begin(38400);
   delay(100);
+  LCD.begin(20, 4);
 }
 
 void loop() {
   if (Serial.available() > 0) {
     Serial.println("Changed");
+    LCD.print((char)Serial.read());
   }
   delay(500);
 }
